c43c037d42b0976b491816f9f9ec09f1636b06da
Tim Quimbo <tquimbo1@gmail.com>
2021-03-30 18:38:00 -0400

    requires files and gems

---
+++ config/environment.rb
@@ -0,0 +1,9 @@
+#access to all the files we need to run our application
+#api file
+require 'pry' #just require for external libs and its absolute path
+require 'httparty' 
+
+require_relative('../lib/api') #for files inside our directory #. goes up a spot (config) #.. goes up to top of cli
+require_relative('../lib/cli') 
+# require_relative('.. /lib/location')
+

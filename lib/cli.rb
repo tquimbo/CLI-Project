708eca7d47be65040b1b0d2b9ad53655ff58d69c
Tim Quimbo <tquimbo1@gmail.com>
2021-03-30 22:32:35 -0400

    Added prompt for zip code.

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,6 +2,12 @@ class CLI
 
     def welcome
         puts "Welcome to the Discover CLI App!"
+        self.zipcode #calling method on an instance
     end  
+
+    def zipcode
+        puts "Please enter your zip code."
+        input = gets.strip
+    end
 
 end

8eaf164f2736fd1729c0421fcb3a3e05a28e4307
Tim Quimbo <tquimbo1@gmail.com>
2021-08-11 14:01:01 -0400

    Updated config/enviroment file

--- CLI-Data-Gem-Portfolio-Project/config/environment.rb
+++ CLI-Data-Gem-Portfolio-Project/config/environment.rb
@@ -2,8 +2,13 @@
 require 'pry' #just require for external libs and its an absolute path
 require 'httparty' 
 
-require_relative('../config/enviroment'
-require_relative('../lib/api') #for files inside our directory #. goes up a spot (config) #.. goes up to top of cli
-require_relative('../lib/cli') 
+require_relative('../config/enviroment')
+require_relative('./lib/restaurant')
+require_relative('./lib/restaurant/cli')
+require_relative('./lib/restaurant/api')  #for files inside our directory #. goes up a spot (config) #.. goes up to top of cli
+require_relative('./lib/restaurant/cuisine')
+require_relative('./lib/restaurant/restaurants')
+require_relative('./lib/restaurant/restaurants')
+
 # require_relative('.. /lib/location')
 
